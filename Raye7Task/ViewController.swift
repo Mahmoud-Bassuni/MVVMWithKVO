42c427fecbe3d11a05ef08e86bbbda920820cdde
mahmoud gaber <mahmoudbasuni90@gmail.com>
2019-05-28 00:47:26 +0200

    Initial Commit

---
+++ Raye7Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Raye7Task
+//
+//  Created by Bassuni on 5/28/19.
+//  Copyright © 2019 Bassuni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
